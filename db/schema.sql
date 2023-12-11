dda43629ce38d307336230d5185bcd034d3d312d
unknown <lizzybumpers11@gmail.com>
2023-12-10 21:20:29 -0600

    started sql files/started writing statements

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IS EXISTS departments_db;
+CREATE DATABASE departments_db;
+
+USE departments_db;
+
+CREATE TABLE departments_db (
+    id INT NOT NULL AUTO_CORRECT PRIMARY KEY,
+    
+)
