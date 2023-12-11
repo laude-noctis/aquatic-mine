7233ab0861efd9200c7dd37ee63a9e48d2dff018
unknown <lizzybumpers11@gmail.com>
2023-12-11 08:53:31 -0600

    added tables to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,24 @@
-DROP DATABASE IS EXISTS departments_db;
-CREATE DATABASE departments_db;
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
 
-USE departments_db;
+USE business_db;
 
-CREATE TABLE departments_db (
-    id INT NOT NULL AUTO_CORRECT PRIMARY KEY,
-    
-)
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
