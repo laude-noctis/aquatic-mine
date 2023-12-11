9eb3ccea8a92dc853db9ac24249fb9995110b875
unknown <lizzybumpers11@gmail.com>
2023-12-11 09:42:57 -0600

    added foreign keys to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -12,13 +12,15 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
+    roles_id INT,
+    FOREIGN KEY (roles_id) REFERENCES roles(id),
     manager_id INT
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,21 +5,21 @@ VALUES ('Sales'),
        ('IT'),
        ('Human Resources');
 
-INSERT INTO roles (title, salary) VALUES
-('Sales Manager', 80000.00),
-('Account Executive', 90000.00),
-('Sales Representative', 75000.00),
-('Marketing Coordinator', 80000.00),
-('Marketing Manager', 70000.00),
-('Social Media Manager', 68000.00),
-('Financial Analyst', 85000.00),
-('Financial Controller', 130000.00),
-('Accountant', 75000.00),
-('Network Administrator', 58000.00),
-('Systems Analyst', 90000.00),
-('Support Specialist', 78000.00),
-('HR Coordinator', 60000.00),
-('HR Manager', 75000.00);
+INSERT INTO roles (title, salary) 
+VALUES  ('Sales Manager', 80000.00),
+        ('Account Executive', 90000.00),
+        ('Sales Representative', 75000.00),
+        ('Marketing Coordinator', 80000.00),
+        ('Marketing Manager', 70000.00),
+        ('Social Media Manager', 68000.00),
+        ('Financial Analyst', 85000.00),
+        ('Financial Controller', 130000.00),
+        ('Accountant', 75000.00),
+        ('Network Administrator', 58000.00),
+        ('Systems Analyst', 90000.00),
+        ('Support Specialist', 78000.00),
+        ('HR Coordinator', 60000.00),
+        ('HR Manager', 75000.00);
 
 INSERT INTO employees (first_name, last_name)
 VALUES  ('Jill', 'Valentine'),
