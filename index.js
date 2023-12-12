2efb96430926191557bb014306afc7508944f7a8
unknown <lizzybumpers11@gmail.com>
2023-12-12 11:40:47 -0600

    created mysql connection and trying to get the first query to work

--- index.js
+++ index.js
@@ -1,67 +1,95 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'name',
-            message: 'Choose an option:',
-            choices: [
-                {
-                    name: 'view all departments',
-                    value: 'allDepartments',
-                    action: () => {
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
 
-                    }
-                },
-                {
-                    name: 'view all roles',
-                    value: 'allRoles',
-                    action: () => {
-                        
-                    }
-                },
-                {
-                    name: 'view all employees',
-                    value: 'allEmployees',
-                    action: () => {
-                        
-                    }
-                },
-                {
-                    name: 'add a department',
-                    value: 'addDepartment',
-                    action: () => {
-                        
-                    }
-                },
-                {
-                    name: 'add a role',
-                    value: 'addRole',
-                    action: () => {
-                        
-                    }
-                },
-                {
-                    name: 'add an employee',
-                    value: 'addEmployee',
-                    action: () => {
-                        
-                    }
-                },
-                {
-                    name: 'update an employee role',
-                    value: 'updateEmployee',
-                    action: () => {
-                        
-                    }
-                },
-                {
-                    name: 'exit',
-                    value: 'quit',
-                    action: () => {
-                        
-                    }
-                }],
-        },
-    ])
+connection.connect((error) => {
+    if (error) {
+        console.error('Error connecting to the database:', error);
+    } else {
+        console.log('Connected to the database!');
+        startPrompt();
+    }
+});
+
+function startPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: 'Choose an option:',
+                choices: [
+                    {
+                        name: 'view all departments',
+                        value: 'allDepartments',
+                        action: () => {
+                            const query = 'SELECT * FROM departments ORDER BY name ASC';
+                            connection.query(query, (error, results) => {
+                                if (error) {
+                                    console.error(error);
+                                } else {
+                                    console.log(results);
+                                }
+                                connection.end();
+                            });
+                        }
+                    },
+                    {
+                        name: 'view all roles',
+                        value: 'allRoles',
+                        action: () => {
+
+                        }
+                    },
+                    {
+                        name: 'view all employees',
+                        value: 'allEmployees',
+                        action: () => {
+
+                        }
+                    },
+                    {
+                        name: 'add a department',
+                        value: 'addDepartment',
+                        action: () => {
+
+                        }
+                    },
+                    {
+                        name: 'add a role',
+                        value: 'addRole',
+                        action: () => {
+
+                        }
+                    },
+                    {
+                        name: 'add an employee',
+                        value: 'addEmployee',
+                        action: () => {
+
+                        }
+                    },
+                    {
+                        name: 'update an employee role',
+                        value: 'updateEmployee',
+                        action: () => {
+
+                        }
+                    },
+                    {
+                        name: 'exit',
+                        value: 'quit',
+                        action: () => {
+
+                        }
+                    }],
+            },
+        ])
+}
